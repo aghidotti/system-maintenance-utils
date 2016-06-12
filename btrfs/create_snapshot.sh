1dd44c7a0b93c4075e8a494f599077f7afd5ce8b
Alessandro Ghidotti Piovan <aghidotti@gmail.com>
2016-06-12 21:08:26 +0200

    Increase portability
    Removed keyword "function" and renamed function name removing dots

--- btrfs/create_snapshot.sh
+++ btrfs/create_snapshot.sh
@@ -5,24 +5,24 @@ ROOTDEV="/dev/system/root"
 SUBVOL="@"
 
 
-function broadcast.info {
+broadcast_info() {
 	echo $1 >&1
 }
 
-function broadcast.error {
+broadcast_error() {
 	echo $1 >&2
 }
 
-function check_superuser {
+check_superuser() {
 	if [ $EUID -ne 0 ]; then
-		broadcast.error "Error: $0 must be executed as root"
+		broadcast_error "Error: $0 must be executed as root"
 		exit 1
 	fi
 }
 
-function check_retval {
+check_retval() {
 	if [ $1 -ne 0 ]; then
-		broadcast.error "Command exited with status $1: exiting"
+		broadcast_error "Command exited with status $1: exiting"
 		exit $1
 	fi
 }
@@ -33,7 +33,7 @@ check_superuser
 
 
 if [ -z "$(command -v btrfs)" ]; then 
-	broadcast.error "Error: btrfs command missing: install btrfs-tools"
+	broadcast_error "Error: btrfs command missing: install btrfs-tools"
 	exit 1
 fi
 
@@ -41,23 +41,23 @@ if [ $# -gt 0 ] && [ -n "$1" ]; then
 	SUBVOL="$1"
 fi
 
-broadcast.info "Creating temporary directory $tmpmnt"
+broadcast_info "Creating temporary directory $tmpmnt"
 tmpmnt=$(mktemp -d)
 check_retval $?
 
-broadcast.info "Mounting top-level subvolume"
+broadcast_info "Mounting top-level subvolume"
 mount $ROOTDEV $tmpmnt/
 check_retval $?
 
-broadcast.info "Creating snapshot of subvolume $SUBVOL"
+broadcast_info "Creating snapshot of subvolume $SUBVOL"
 btrfs sub snap -r $tmpmnt/$SUBVOL $tmpmnt/@$SUBVOL-$TIMESTAMP
 check_retval $?
 
-broadcast.info "Unmounting top-level subvolume"
+broadcast_info "Unmounting top-level subvolume"
 umount $tmpmnt/
 check_retval $?
 
-broadcast.info "Removing temporary directory $tmpmnt"
+broadcast_info "Removing temporary directory $tmpmnt"
 rmdir $tmpmnt
 check_retval $?
 
